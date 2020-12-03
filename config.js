af79329c03dc9d40776cc8b021d4396086b41ec1
Dmitriy Karmalita <dmitriy.ka@maxitechs.com>
2020-12-03 18:34:53 +0200

    [Clean]

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const headless = true;
 module.exports = {
   port: 3030,
   jestTimeout: 20000000,
-  slowMo: 0, //headless ? 0 : 50,
-  headless: false,
+  slowMo: headless ? 0 : 50,
+  headless,
 }
